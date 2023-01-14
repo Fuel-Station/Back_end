f294acdfebe357660ad567d706a9af230c47831a
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-01-15 01:35:38 +0530

    add vehicles router

--- routers/vehicles.js
+++ routers/vehicles.js
@@ -1,7 +1,7 @@
 const {Vehicle}=require('../model/vehicle')
 const express=require('express');
 const router =express.Router();
-
+//add vehicle
 router.post('/register', async(req, res) => {
     let vehicle = Vehicle({
         name: req.body.name,
@@ -13,7 +13,19 @@ router.post('/register', async(req, res) => {
     if(!vehicle){
         return res.status(404).send('The vehicle cannot be created!');
     }
+    res.send(vehicle);
+    
 
+});
+
+//get vehicel
+router.get(`/get`,async(req,res)=>{
+    const vehicle=await Vehicle.find();
+
+    if(!vehicle){
+        res.status(500).json({success:false});
+    }
     res.send(vehicle);
 })
+
 module.exports = router;
