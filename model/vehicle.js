5d3309205e5dabf6315966342356687409d3af62
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-01-15 00:15:39 +0530

    add vehicle  schema and routes

---
+++ model/vehicle.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+
+const vehicleSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    fueltype:{
+        type:String,
+        required:true
+    },
+    
+})
+exports.Vehicle=mongoose.model('Vehicle',vehicleSchema)
