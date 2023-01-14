20a4a0b51392b85f56e130d35a1418ccc8bf2bb8
Chathuranga97lj <chathuit97@gmail.com>
2023-01-14 18:17:57 +0530

    create base

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ npm i nodemon
 ## install express
 
 npm i express
+
+## install dotenv
+
+npm i dotenv
+
+## install morgan
+
+npm i morgan
+
+## install mongoose
+
+npm i mongoose
+
+## install cors
+
+npm i cors
+
+## body parser
+
+npm i body-parser
--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express();
+require('dotenv/config');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('<h1>Fuel app </h1>');
+app.use(cors());
+// http request
+app.options('*', cors());
+
+// for indentify post request json file format (Middleware)
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
+})
+.then(() => {
+    console.log('Database connection is ready......');
+})
+.catch((err) => {
+    console.log(err);
 })
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+    res.send('<h1>Fuel app create </h1>');
+})
+
+
+app.listen(process.env.PORT, () => {
     console.log('Server is listening.....')
 })
