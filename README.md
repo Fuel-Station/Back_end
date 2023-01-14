c619d8272163bc261b30ee9a365082e9f43b05ed
Chathuranga97lj <chathuit97@gmail.com>
2023-01-14 17:48:57 +0530

    set local host

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Back_end
+
 Using node.js
+
+## init npm
+
+npm init -y
+
+## install node mooduels
+
+npm i nodemon
+
+## install express
+
+npm i express
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Server is running test")
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Fuel app </h1>');
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening.....')
+})
